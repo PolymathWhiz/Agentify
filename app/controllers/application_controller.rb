a757f0133273b9860161f97b67bb255197109218
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2017-07-12 23:50:28 +0100

    permission now works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+ 	def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:business_name, :agency_type, :phno, :address, :city,
+                                                              :state, :business_website, :requester_name, :requester_email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:business_name, :agency_type, :phno, :address, :city,
+    																										:state, :business_website, :requester_name, :requester_email])
+  end
+
+  def render_404
+    respond_to do |format|
+      format.html do
+        render file: Rails.root.join("public", "404"), layout: false, status: "404"
+      end
+      format.any { head :not_found }
+    end
+  end
+
 end
--- app/models/business.rb
+++ app/models/business.rb
@@ -8,4 +8,11 @@ class Business < ApplicationRecord
   											 :requester_name, :requester_email, :agency_type
 
   # TODO Validate the various formats of inputs
+
+  def active_for_authentication?
+    #remember to call the super
+    #then put our own check to determine "active" state using
+    #our own "is_active" column
+    super and self.activated?
+  end
 end
