efcdd898c2555b6812f036ff1d30c035d537eb77
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2017-07-24 20:54:10 +0100

    basic activation of account works

--- app/controllers/admin/businesses_controller.rb
+++ app/controllers/admin/businesses_controller.rb
@@ -1,25 +1,26 @@
 class Admin::BusinessesController < Admin::BaseController
+  before_action :set_business, only: [:update, :destroy]
 
   def business_request
+    @unactivated_businesses = Business.where(activated: false).page(params[:page]).per(30)
   end
 
   def update
-    @business = Business.find(params[:id])
     # TODO toggle the activated status for businesses 
     @business.toggle(:activated)
     if @business.save
       flash[:success] = "Successfully activated #{@business.business_name}'s business."
       # TODO send mail to business to notify it has been added
+      redirect_to admin_requests_path
     else
       flash[:danger] = "Unable to activate #{@business.business_name}'s business."
     end
   end
 
   def destroy
-    @business = Business.find(params[:id])
     if @business.destroy
       flash[:success] = "Successfully deleted business"
-      redirect_to admin_users_path
+      redirect_to admin_requests_path
     else 
       flash[:warning] = "Problem encountered while deleting the current business."
     end
@@ -28,6 +29,10 @@ class Admin::BusinessesController < Admin::BaseController
   private
     def business_params
       params.require(:business).permit()
+    end
+
+    def set_business
+      @business = Business.friendly.find(params[:id])
     end
 
 end
