078611a40b4fa2b014c161c01c56f3a5109bdf9b
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2017-08-03 20:23:13 +0100

    changed devise header

--- app/mailers/my_mailer.rb
+++ app/mailers/my_mailer.rb
@@ -3,9 +3,4 @@ class MyMailer < Devise::Mailer
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
   default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
 
-  def confirmation_instructions(record, token, opts={})
-    headers["display_name"] = "Agentify"
-    super
-  end
-
 end
