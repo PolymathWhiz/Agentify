bacae22a9bbfe2799b7ded32fd3d3c940037a611
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2017-08-12 14:53:07 +0100

    changed the motto

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title)
-      base_title = "Agentify - Find verified agents all over Nigeria."
+      base_title = "Agentify - Connecting people to verified agents all over Nigeria."
       page_title.empty? ? base_title : "Agentify - #{page_title}"
     end
 end
